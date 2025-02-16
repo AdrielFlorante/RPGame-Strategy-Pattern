f93a4e6c62e2e2582f53eb02188f439752955de2
Prinz-Heinz <prinzheinzketchup@gmail.com>
2025-02-16 16:32:20 +0800

    Added Archer

--- src/GameApp.java
+++ src/GameApp.java
@@ -2,11 +2,15 @@ public class GameApp {
     public static void main(String[] args) {
         Character knight = new Knight();
         Character wizard = new Wizard();
+        Character archer = new Archer();
 
         knight.attack();
         knight.defend();
 
         wizard.attack();
         wizard.defend();
+
+        archer.attack();
+        archer.defend();
     }
 }
