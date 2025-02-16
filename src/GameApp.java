870cb4910b4a6890456c949a3357fed851f23948
Prinz-Heinz <prinzheinzketchup@gmail.com>
2025-02-16 16:28:13 +0800

    Added Wizard

--- src/GameApp.java
+++ src/GameApp.java
@@ -1,9 +1,12 @@
 public class GameApp {
     public static void main(String[] args) {
         Character knight = new Knight();
+        Character wizard = new Wizard();
 
         knight.attack();
         knight.defend();
 
+        wizard.attack();
+        wizard.defend();
     }
 }
